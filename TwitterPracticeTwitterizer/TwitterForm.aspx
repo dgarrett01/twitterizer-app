28f43613a2c02c3f50fc5c40fbfad417e0541836
dgarrett01 <david.garrett01@gmail.com>
2018-05-19 23:28:09 -0400

    Initial upload

---
+++ TwitterPracticeTwitterizer/TwitterForm.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TwitterForm.aspx.cs" Inherits="TwitterPracticeTwitterizer.TwitterForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="tbTweet" runat="server" Height="69px" MaxLength="140" Width="445px"></asp:TextBox>
+        <asp:Button ID="btnTweet" runat="server" OnClick="btnTweet_Click" Text="Tweet" Width="90px" />
+        <asp:Button ID="btnQuit" runat="server" OnClick="btnQuit_Click" Text="Quit" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ TwitterPracticeTwitterizer/TwitterForm.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tweetinvi;
+using System.Windows.Forms;
+
+namespace TwitterPracticeTwitterizer
+{
+    public partial class TwitterForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        protected void btnTweet_Click(object sender, EventArgs e)
+        {
+            // Private key information from Twitter required in order to post to your account
+            string ConsumerKey = "*********";
+            string ConsumerSecret = "*******************************";
+            string Token = "**********************************";
+            string TokenSecret = "***********************************";
+
+            // Set up your credentials (https://apps.twitter.com)
+            Auth.SetUserCredentials(ConsumerKey, ConsumerSecret, Token, TokenSecret);
+
+            // Publish the Tweet "Hello World" on your Timeline
+            Tweet.PublishTweet(tbTweet.Text.ToString());
+        }
+
+        protected void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
